04efd14e8623ef2a406f0b76f4166811b6fb79e7
Zealtiel <zealtiel@outlook.com>
2025-03-27 11:40:29 -0500

    Initial commit and establish folder structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import NotFound from "./pages/NotFound/NotFound";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <h1>Hello World!</h1>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import "./Homepage.scss";
+
+export default function Homepage() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+    </div>
+  );
+}
