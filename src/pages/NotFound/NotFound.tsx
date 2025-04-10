670a9d5b05f1d50edf67598fd9968c7b8f96a3a1
Zealtiel Zuniga <zealtiel@outlook.com>
2025-04-10 14:08:02 -0500

    Style notFound.

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import "./NotFound.scss";
+import { MouseEventHandler } from "react";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  const handleNav: MouseEventHandler<HTMLButtonElement> = () => {
+    navigate(-1);
+    return;
+  };
+
   return (
-    <div>
-      <h1>
-        This page is still under construction. Thank you for your patience.
-      </h1>
+    <div className="notFound">
+      <h2 className="notFound__header">
+        This page is under indefinite construction as this is a fictional
+        website. Thank you for your patience.
+      </h2>
+      <button onClick={handleNav} className="notFound__button">
+        Click To Go Back
+      </button>
     </div>
   );
 }
